286479a945eb7fe577d4778271c7664793e56661
rafalmalenta <rmalenta@wp.pl>
2019-07-15 09:30:37 +0200

    finished task2

--- src/components/EditButton.vue
+++ src/components/EditButton.vue
@@ -1,8 +1,14 @@
 <template>
-    <button>Edytuj</button>
+    <button v-on:click="$emit('edit-employee',id)">Edytuj</button>
 </template>
 <script>
 export default {
     name:"EditButton",
+    props:{ 
+        id : Number 
+    },
 }
 </script>
+<style lang="scss" scoped>
+
+</style>
